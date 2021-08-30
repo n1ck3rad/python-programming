b4abcd8b903038091d6cf206a250f81c010a0563
n1ck3rad <89400243+n1ck3rad@users.noreply.github.com>
2021-08-30 09:52:19 -0500

    direct upload
    numeric operations

---
+++ second.py
@@ -0,0 +1,10 @@
+inputOne=2
+inputTwo=5
+result= inputOne + inputTwo
+print("the sum of given numbers is" , result)
+result= inputOne - inputTwo
+print("the difference of given numbers is" , result)
+result= inputOne * inputTwo
+print("the product of given numbers is" , result)
+result= inputOne / inputTwo
+print("the quotient of given numbers is" , result)
